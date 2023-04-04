223e5a61025f616c3720aa6c3683729fbbb2a175
PPTide <52575064+PPTide@users.noreply.github.com>
2023-04-04 10:03:01 +0200

    Framework to add different Idle and Run anims

--- Player.gd
+++ Player.gd
@@ -1,24 +1,37 @@
 extends CharacterBody2D
 
-func _physics_process(delta):
-	$AnimatedSprite2D.play("Idle")
+var current_anim = ""
+var old_direction = ""
 
+func _physics_process(delta):
 	var direction = Vector2.ZERO
 	
+	#TODO: Sind Idle animationen
 	if Input.is_action_pressed("ui_right"):
 		direction.x = 1
-		$AnimatedSprite2D.play("Run_Right")
-	if Input.is_action_pressed("ui_left"):
+		animNoStart("Run_Right")
+		old_direction = "_Right"
+	elif Input.is_action_pressed("ui_left"):
 		direction.x = -1
-		$AnimatedSprite2D.play("Run_Left")
-	if Input.is_action_pressed("ui_down"):
+		animNoStart("Run_Left")
+		old_direction = "_Left"
+	elif Input.is_action_pressed("ui_down"):
 		direction.y = 1
-		$AnimatedSprite2D.play("Run_Down")
-	if Input.is_action_pressed("ui_up"):
+		animNoStart("Run_Down")
+		old_direction = "_Down"
+	elif Input.is_action_pressed("ui_up"):
 		direction.y = -1
-		$AnimatedSprite2D.play("Run_Up")
+		animNoStart("Run_Up")
+		old_direction = "_Up"
+	else:
+		animNoStart("Idle"+old_direction)
 		
 		
 	self.velocity = direction * 40
 	
 	self.move_and_slide()
+
+func animNoStart(name):
+	if current_anim != name:
+		current_anim = name
+		$AnimatedSprite2D.play(name)
