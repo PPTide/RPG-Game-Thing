6e9d06386a054a3e9025cb4a0c468e75ed37ef10
PPTide <52575064+PPTide@users.noreply.github.com>
2023-05-02 10:12:59 +0200

    idk...

--- addons/dialogue_manager/DialogueManager.cs
+++ addons/dialogue_manager/DialogueManager.cs
@@ -6,19 +6,19 @@ namespace DialogueManagerRuntime
 {
   public partial class DialogueManager : Node
   {
-    public static async Task<RefCounted> GetNextDialogueLine(Resource dialogueResource, string key = "0", Array<Variant> extraGameStates = null)
-    {
-      var dialogueManager = Engine.GetSingleton("DialogueManager");
-      dialogueManager.Call("_bridge_get_next_dialogue_line", dialogueResource, key, extraGameStates ?? new Array<Variant>());
-      var result = await dialogueManager.ToSignal(dialogueManager, "bridge_get_next_dialogue_line_completed");
+	public static async Task<RefCounted> GetNextDialogueLine(Resource dialogueResource, string key = "0", Array<Variant> extraGameStates = null)
+	{
+	  var dialogueManager = Engine.GetSingleton("DialogueManager");
+	  dialogueManager.Call("_bridge_get_next_dialogue_line", dialogueResource, key, extraGameStates ?? new Array<Variant>());
+	  var result = await dialogueManager.ToSignal(dialogueManager, "bridge_get_next_dialogue_line_completed");
 
-      return (RefCounted)result[0];
-    }
+	  return (RefCounted)result[0];
+	}
 
 
-    public static void ShowExampleDialogueBalloon(Resource dialogueResource, string key = "0", Array<Variant> extraGameStates = null)
-    {
-      Engine.GetSingleton("DialogueManager").Call("show_example_dialogue_balloon", dialogueResource, key, extraGameStates ?? new Array<Variant>());
-    }
+	public static void ShowExampleDialogueBalloon(Resource dialogueResource, string key = "0", Array<Variant> extraGameStates = null)
+	{
+	  Engine.GetSingleton("DialogueManager").Call("show_example_dialogue_balloon", dialogueResource, key, extraGameStates ?? new Array<Variant>());
+	}
   }
 }
