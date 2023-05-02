a927abde2d39dcc4a671e4b2bd561fa8e390ea44
PPTide <52575064+PPTide@users.noreply.github.com>
2023-05-02 18:58:44 +0200

    Main Menu Stuff

---
+++ main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var sgb: Button = $HBoxContainer/VBoxContainer/StartGameButton
+@onready var qgb: Button = $HBoxContainer/VBoxContainer/QuitGameButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	sgb.pressed.connect(_sgb_pressed)
+	qgb.pressed.connect(_qgb_pressed)
+
+func _sgb_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+	
+func _qgb_pressed():
+	get_tree().quit()
