529108f69c17bb6bb784cf36ce12d92348e5cddd
PPTide <52575064+PPTide@users.noreply.github.com>
2023-05-04 15:51:50 +0200

    Add basic Pause Menu

---
+++ pause_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().paused = !get_tree().paused
+		self.visible = !self.visible
