518390c36e80adcae3104ab915413550c6209b8b
PPTide <52575064+PPTide@users.noreply.github.com>
2023-05-04 16:11:47 +0200

    add settings screen

--- pause_menu.gd
+++ pause_menu.gd
@@ -3,8 +3,14 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hide_all_children()
+	$Pause.show()
+	
+	$Pause/Button.pressed.connect(_settings_button_pressed)
 
+func hide_all_children():
+	for child in self.get_children():
+		child.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,5 +19,10 @@ func _process(delta):
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
+		_ready()
 		get_tree().paused = !get_tree().paused
 		self.visible = !self.visible
+
+func _settings_button_pressed():
+	hide_all_children()
+	$Settings.show()
