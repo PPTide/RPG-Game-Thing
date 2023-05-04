2b3e16b28dc74fcf45d972693fcdeeb190ef5ad8
PPTide <52575064+PPTide@users.noreply.github.com>
2023-05-04 17:15:58 +0200

    Make languages Translateable

--- Menus/settings_menu.gd
+++ Menus/settings_menu.gd
@@ -2,12 +2,17 @@ extends VBoxContainer
 
 @onready var LangSelector:OptionButton = $LangSelector
 
+var langs:Array[String] = [tr("English"), tr("German")]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	load_lang()
 	LangSelector.item_selected.connect(_lang_selected)
 
 func load_lang():
+	LangSelector.clear()
+	for l in langs:
+		LangSelector.add_item(l)
 	var lang = TranslationServer.get_locale()
 	if lang.begins_with("de"):
 		LangSelector.select(1)
