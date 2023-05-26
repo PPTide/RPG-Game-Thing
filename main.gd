b6bf30cdcc3463ceda5575b4e808b3e2c9d37cfd
PPTide <52575064+PPTide@users.noreply.github.com>
2023-05-26 09:59:32 +0200

    Fade to black while changing map

--- main.gd
+++ main.gd
@@ -3,10 +3,19 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_change_map()
+	var map = load(State.game.map).instantiate()
+	add_child(map)
 	State.game.map_changed.connect(_change_map)
+	$CanvasLayer/Black.show()
+	$CanvasLayer/Black.color = Color.TRANSPARENT
 
 func _change_map():
+	fade_to_black(2)
 	remove_child($TileMap)
 	var map = load(State.game.map).instantiate()
 	add_child(map)
+
+func fade_to_black(time:float):
+	var tween = get_tree().create_tween()
+	tween.tween_property($CanvasLayer/Black, "color", Color.BLACK, 1)
+	tween.tween_property($CanvasLayer/Black, "color", Color.TRANSPARENT, 1)
